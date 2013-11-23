f937afde5cb6c00b9aad79aa1aacab2882d8f607
robinnoguier <robin.noguier@hotmail.fr>
2013-11-23 17:31:22 +0100

    Page single event ( à reprendre)

---
+++ tour13.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+     <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/less/main.css">
+        <link rel="stylesheet" href="css/css/main.css"> 
+        <link href="css/css/font-awesome.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    
+        <header style="height:75px;opacity:1;">
+            <h1><a href=""> World Wide <span> Writters</span></a></h1>
+            <a class="Participez" href="">Participez</a>
+            <a href="">Artist of the week</a>
+
+        </header>
+        <section class="single-event">
+        <div class="cadre-noir">
+          <h1 class="titre_event">Tour Paris XIII </h1>
+          <p class="description_event"> La Tour Paris 13 et le street art ? C’est un événement énorme, inattendu, tenu secret depuis des mois. 
+            Jamais en France un lieu n’avait réuni autant d’artistes de street art pour une exposition temporaire, éphémère à couper le souffle.
+             Pendant des mois, dans le silence absolu, Mehdi, de la galerie Itinerrance (figure de proue et rassembleur incontournable de talents de street art dans la capitale) en coordination 
+             avec la mairie du 13e arrondissement de Paris (et son maire Jérôme Coumet, passionné de street art) ont réuni dans une tour du front de seine (Métro Quai de la Gare) 100 artistes du monde entier (16 nationalités) 
+             en leur donnant une carte blanche créative. Objectif artistique : revisitez les appartements vides de ce bâtiment qui sera démoli en fin d’année. La crème de la crème du street art s’est exprimée sur 9 étages (et le sous-sol!) 
+             avec le talent qu’on leur connaît! 
+         Tarif : Free                                     Follow me on: </br>
+            Adresse:
+            Site:
+            Ouverture:</p>
+     
+             <img src="images/paris/events/event-1/flickr.jpg" class="flickr" alt="api-flickr">
+
+        </div>
+
+
+        </section>
+
+    </body>
+</html>
